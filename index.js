4609af1ab43fc0eaf17a859f311ce5cbcad30020
Aliona Sipchuk <Aliona_Sipchuk@epam.com>
2018-04-23 09:59:57 +0300

    home task #1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { User, Product } from './models'
 import config from './config'
 
-console.log(name,'111111111')
+console.log(config.name)
 
 const user = new User()
 const product = new Product()
